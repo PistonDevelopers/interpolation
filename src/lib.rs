6dee930eebe631e15940461ea1e88706d0bf97ed
Aleksey Kuznetsov <zummenix@gmail.com>
2015-01-13 22:52:31 +0500

    Replaced ease functions with 'Ease' trait.

--- src/lib.rs
+++ src/lib.rs
@@ -2,22 +2,28 @@
 
 //! Interpolation algorithms.
 //!
-//! Interpolation is used in animation, 
+//! Interpolation is used in animation,
 //! to describe smooth shapes and to make transitions.
-//! Any object that fullfill certain mathematical 
+//! Any object that fullfill certain mathematical
 //! properties can be interpolated.
-//! A common technique is using one ore more 'numbers' 
+//! A common technique is using one ore more 'numbers'
 //! controlling the mixture of states.
-//! The choice of interpolation algorithm depends often 
+//! The choice of interpolation algorithm depends often
 //! on the circumstances where it used.
 
 use std::num::Float;
 
-pub mod ease;
+pub use ease::{
+    Ease,
+    EaseFunction,
+};
+
+mod ease;
+
 
 /// Performs linear interpolation.
 /// A linear interpolation consists of two states 'a' and 'b'.
-/// The 't' variable is a factor between 0 and 1 that 
+/// The 't' variable is a factor between 0 and 1 that
 /// gives weight to 'a' or 'b'.
 /// When 't' is zero then 'a' has full weight.
 /// When 't' is one then 'b' has full weight.
@@ -33,9 +39,9 @@ pub fn lerp<T: Copy + Float>(a: T, b: T, t: T) -> T {
 /// <a href="http://en.wikipedia.org/wiki/B%C3%A9zier_curve">Beziér Curve at Wikipedia</a>
 #[inline(always)]
 pub fn quad_bez<T: Copy + Float>(
-    x0: T, 
-    x1: T, 
-    x2: T, 
+    x0: T,
+    x1: T,
+    x2: T,
     t: T
 ) -> T {
     let x_0_1 = lerp(x0, x1, t);
@@ -50,10 +56,10 @@ pub fn quad_bez<T: Copy + Float>(
 /// <a href="http://en.wikipedia.org/wiki/B%C3%A9zier_curve">Beziér Curve at Wikipedia</a>
 #[inline(always)]
 pub fn cub_bez<T: Copy + Float>(
-    x0: T, 
-    x1: T, 
-    x2: T, 
-    x3: T, 
+    x0: T,
+    x1: T,
+    x2: T,
+    x3: T,
     t: T
 ) -> T {
     let x_0_2 = quad_bez(x0, x1, x2, t);
